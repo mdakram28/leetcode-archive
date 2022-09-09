3bd5544df470f4efd8979e4f17401e5d8976af2e
Mohd Akram Ansari <mdakram28@gmail.com>
2022-09-09 01:34:51 +0000

    Sync LeetCode submission - First Bad Version (cpp)

--- my-folder/problems/first_bad_version/solution.cpp
+++ my-folder/problems/first_bad_version/solution.cpp
@@ -4,16 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long left=1;
-        long right=n;
-        while(left < right) {
-            int mid = (left + right)/2;
-            if (isBadVersion(mid)) {
-                right = mid;
+        long l = 1;
+        long r = n;
+        while(l<r) {
+            long mid = (l+r)/2;
+            if(isBadVersion(mid)) {
+                r = mid;
             } else {
-                left = mid+1;
+                l = mid+1;
             }
         }
-        return (int)left;
+        return l;
     }
 };
