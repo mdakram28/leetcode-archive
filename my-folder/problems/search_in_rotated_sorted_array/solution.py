e5d094e1efe0e390ceb8ccbd3e84cafc3e638bd3
Mohd Akram Ansari <mdakram28@gmail.com>
2023-03-17 07:47:35 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (python3)

--- my-folder/problems/search_in_rotated_sorted_array/solution.py
+++ my-folder/problems/search_in_rotated_sorted_array/solution.py
@@ -1,27 +1,33 @@
 class Solution:
-    def findOffset(self, nums):
-        l = 0
-        r = len(nums)
-        while l < r:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums)-1
+        while (end-start)>1 and nums[end] < nums[start]:
+            # print(f"{start=}, {end=}")
+
+            mid = (start+end)//2
+            if nums[mid] < nums[start]:
+                end = mid
+            else:
+                start = mid
+        
+        offset = end if nums[end] < nums[start] else start
+        # print(f"{offset=}")
+
+        idx = lambda i: (i+offset)%len(nums)
+
+        l, r = 0, len(nums)-1
+        while l<r:
             mid = (l+r)//2
-            if nums[mid] > nums[l]:
-                l = mid
-            else:
-                r = mid
-        return l+1
-    def search(self, nums: list[int], target: int) -> int:
-        offset = self.findOffset(nums)
-        length = len(nums)
-        l = 0
-        r = len(nums)
-        i  = lambda x: (x+offset)%length
-
-        print("Offset : ", offset)
-
-        while l < r:
-            mid = (l+r) // 2
-            if nums[i(mid)] < target:
+            mid_val = nums[idx(mid)]
+            # print(f"{l=}, {r=}, {mid_val=}")
+            if mid_val < target:
                 l = mid+1
-            else:
+            elif mid_val >= target:
                 r = mid
-        return i(l) if nums[i(l)] == target else -1
+        
+        if nums[idx(l)] == target:
+            return idx(l)
+        else:
+            return -1
+        
+
