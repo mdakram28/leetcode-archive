c98e5af1d7d92bfc65bc4156ac4beb9d5d0a14b4
Mohd Akram Ansari <mdakram28@gmail.com>
2023-03-26 12:31:41 +0000

    Sync LeetCode submission - Rotate Image (python3)

--- my-folder/problems/rotate_image/solution.py
+++ my-folder/problems/rotate_image/solution.py
@@ -1,20 +1,28 @@
 class Solution:
-    def rotate(self, matrix: list[list[int]]) -> None:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        size = len(matrix)
-        for r in range(size//2):
-            for c in range (r, size-r-1):
-                r1, c1 = r, c
-                r2, c2 = c, size-r-1
-                r3, c3 = size-r-1, size-c-1
-                r4, c4 = size-c-1, r
-                # print(f"({r1}, {c1}) -> ({r2}, {c2})")
-                # print(f"({r2}, {c2}) -> ({r3}, {c3})")
-                # print(f"({r3}, {c3}) -> ({r4}, {c4})")
-                # print(f"({r4}, {c4}) -> ({r1}, {c1})")
-                # print("-------------------")
-                matrix[r][c], matrix[c][size-r-1], matrix[size-r-1][size-c-1], matrix[size-c-1][r] = [
-                    matrix[size-c-1][r], matrix[r][c], matrix[c][size-r-1], matrix[size-r-1][size-c-1]
-                ]
+        n = len(matrix)
+        left, top = 0, 0
+        right, bottom = n-1, n-1
+        count = n-1
+
+        while count > 0:
+            for i in range(count):
+                (
+                    matrix[top][left+i], 
+                    matrix[top+i][right],
+                    matrix[bottom][right-i],
+                    matrix[bottom-i][left]
+                ) = (
+                    matrix[bottom-i][left],
+                    matrix[top][left+i], 
+                    matrix[top+i][right],
+                    matrix[bottom][right-i],
+                )
+            left += 1
+            top += 1
+            right -= 1
+            bottom -= 1
+            count -= 2
