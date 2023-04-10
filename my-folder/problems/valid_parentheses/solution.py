97045b8ce528301ae6489660e49d4396652c7a8f
Mohd Akram Ansari <mdakram28@gmail.com>
2023-04-10 00:41:19 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- my-folder/problems/valid_parentheses/solution.py
+++ my-folder/problems/valid_parentheses/solution.py
@@ -5,13 +5,10 @@ CLOSING = {
 }
 class Solution:
     def isValid(self, s: str) -> bool:
-        b1, b2, b3 = 0, 0, 0
         st = []
         for c in s:
-            if c in ('(', '{', '['):
-                st.append(c)
-            else:
-                if len(st) == 0 or c != CLOSING[st.pop()]:
-                    return False
-
-        return len(st) == 0
+            if c in CLOSING:
+                st.append(CLOSING[c])
+            elif not st or c != st.pop():
+                return False
+        return not st
