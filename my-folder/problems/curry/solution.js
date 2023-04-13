45dbf7ee0821e9b1ca0cdc4ee087abf48b677706
Mohd Akram Ansari <mdakram28@gmail.com>
2023-04-13 04:47:13 +0000

    Sync LeetCode submission - Curry (javascript)

---
+++ my-folder/problems/curry/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function(fn) {
+    return function curried(...args) {
+        if (fn.length === args.length) return fn(...args);
+        else fn = fn.bind(this, ...args);
+        return curried;
+    };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
