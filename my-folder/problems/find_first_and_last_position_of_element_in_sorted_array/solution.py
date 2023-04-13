953ad9e921e17fd9e66d85f7261b3f55046748dc
Mohd Akram Ansari <mdakram28@gmail.com>
2023-04-13 06:34:30 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (python3)

--- my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -1,34 +1,24 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = -1
-        if len(nums) > 0 and nums[0] == target:
-            first = 0
-        else:
-            l = 0
-            r = len(nums)-1
-            while r-l>1:
-                mid = (l+r)//2
-                if nums[mid] >= target:
-                    r = mid
-                else:
-                    l = mid
-            # print(f"first = {r}")
-            if r >= 0 and r < len(nums) and nums[r] == target:
-                first = r
+        l = 0
+        r = len(nums)
+        while (r-l) > 1:
+            mid = (l+r)//2
+            if nums[mid] <= target:
+                l = mid
+            else:
+                r = mid
+        if l >= len(nums) or nums[l] != target:
+            return -1, -1
+        last = l
+
+        l = 0
+        r = last
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid] < target:
+                l = mid+1
+            else:
+                r = mid
         
-        if first != -1:
-            l = first
-            r = len(nums)
-            while r-l>1:
-                mid = (l+r)//2
-                # print(f"l={l}, r={r}, mid={mid}")
-                if nums[mid] <= target:
-                    l = mid
-                else:
-                    r = mid
-            last = l
-            return [first, last]
-        else:
-            return [-1, -1]
-            
-        
+        return l, last
