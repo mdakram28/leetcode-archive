05defbfc363c64d311116d2471659eb07681a6fc
Mohd Akram Ansari <mdakram28@gmail.com>
2023-04-19 07:30:52 +0000

    Sync LeetCode submission - Maximum Level Sum of a Binary Tree (python3)

---
+++ my-folder/problems/maximum_level_sum_of_a_binary_tree/solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        q = Deque([root])
+        max_sum = -float('inf')
+        max_level = -1
+        level = 1
+        while q:
+            s = 0
+            for _ in range(len(q)):
+                node = q.popleft()
+                s += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if s > max_sum:
+                max_level = level
+                max_sum = s
+            level += 1
+        return max_level
