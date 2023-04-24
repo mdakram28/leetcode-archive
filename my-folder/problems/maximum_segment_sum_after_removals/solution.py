f7030e262f77a5d9c22a52edd26f75a9006b380b
Mohd Akram Ansari <mdakram28@gmail.com>
2023-04-24 08:12:53 +0000

    Sync LeetCode submission - Maximum Segment Sum After Removals (python3)

--- my-folder/problems/maximum_segment_sum_after_removals/solution.py
+++ my-folder/problems/maximum_segment_sum_after_removals/solution.py
@@ -1,35 +1,124 @@
+from sortedcontainers import SortedList
+
+class Node:
+    def __init__(self, val, left = None, right = None, parent = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.parent = parent
+        
+    def insert(self, node):
+        if self.val > node.val:
+            if self.left is None:
+                self.left = node
+                node.parent = self
+            else:
+                self.left.insert(node)
+        else:
+            if self.right is None:
+                self.right = node
+                node.parent = self
+            else:
+                self.right.insert(node)
+    
+    def min_val(self):
+        node = self
+        val = None
+        while node:
+            val = node.val
+            node = node.left
+        return val
+    
+    def max_val(self):
+        node = self
+        val = None
+        while node:
+            val = node.val
+            node = node.right
+        return val
+    
+    def successor(self):
+        node = self
+        while node.parent:
+            if node.parent.left == node:
+                return node.parent
+            node = node.parent
+        return None
+    
+    def predecessor(self):
+        node = self
+        while node.parent:
+            if node.parent.right == node:
+                return node.parent
+            node = node.parent
+        return None
+    
+    def print(self, pref='', sign=""):
+        print(f"{pref}━({self.val})")
+        if self.left:
+            self.left.print(pref+" ┃")
+        else:
+            print(f"{pref} ┃━()")
+        if self.right:
+            self.right.print(pref+" ┃")
+        else:
+            print(f"{pref} ┃━()")
+    
 class Solution:
     def maximumSegmentSum(self, nums: List[int], removeQueries: List[int]) -> List[int]:
         n = len(nums)
-        acc = [0]
-        # prefix sum
-        for num in nums:
-            acc.append(acc[-1] + num)
-        
-        # current max sum with boundries
-        q = [(-sum(nums), 0, n - 1)]
-        # removed index
-        rm_idx = []
-        
-        
+        sums = []
         ans = []
-        for idx in removeQueries:
-            bisect.insort(rm_idx, idx)
-            while q:
-                _, s, e = q[0]
-                loc = bisect.bisect_left(rm_idx, s)
-                idx = rm_idx[min(loc, len(rm_idx) - 1)]
-                # if removed index fall between the boundry of largest sum
-                if idx > e or idx < s: break
-                heappop(q)
-                if s < idx:
-                    heappush(q, (-(acc[idx] - acc[s]), s, idx - 1))
-                if e > idx:
-                    heappush(q, (-(acc[e + 1] - acc[idx + 1]), idx + 1, e))
-            if q:
-                ans.append(-q[0][0])
-            else:
-                ans.append(0)
+        freq = defaultdict(int)
+        
+        # root = Node(-1)
+        # root.insert(Node(n))
+        sl = SortedList([-1, n])
+        
+        
+        total = 0
+        for i in range(n):
+            total += nums[i]
+            nums[i] = total
+        
+        nums.append(0)
+        
+        sums.append(-total)
+        freq[total] += 1
+        
+        for rem in removeQueries:
+            # print(sums)
             
+            # node = Node(rem)
+            # root.insert(node)
+            sl.add(rem)
+            i = sl.index(rem)
+            prev_i = sl[i-1]
+            next_i = sl[i+1]
+            
+            # root.print()
+            # print(prev_i, next_i)
+            
+            prev_sum = nums[next_i-1] - nums[prev_i]
+            freq[prev_sum] -= 1
+            
+            curr_sum = nums[rem-1] - nums[prev_i]
+            heappush(sums, -curr_sum)
+            freq[curr_sum] += 1
+            
+            curr_sum = nums[next_i-1] - nums[rem]
+            heappush(sums, -curr_sum)
+            freq[curr_sum] += 1
+            
+            # print(freq)
+            while sums and freq[-sums[0]] == 0:
+                # print(f"{-sums[0]} has been removed")
+                heappop(sums)
+            ans.append(-sums[0] if sums else 0)
         
         return ans
+            
+        
+        
+        
+        
