993bead13a808b9dd9743a55b204e30c5a25dbdf
Mohd Akram Ansari <mdakram28@gmail.com>
2023-04-28 04:01:54 +0000

    Sync LeetCode submission - Alternating Digit Sum (python3)

--- my-folder/problems/alternating_digit_sum/solution.py
+++ my-folder/problems/alternating_digit_sum/solution.py
@@ -1,12 +1,7 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
-        sign = 1
         total = 0
-        i = 0
-        while n:
-            i += 1
-            total += sign*(n%10)
-            n //= 10
+        for c, sign in zip(str(n), cycle((1, -1))):
+            total += sign*int(c)
             sign = -sign
-        
-        return total if i%2 else -total
+        return total
