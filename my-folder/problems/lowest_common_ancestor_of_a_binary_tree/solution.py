0e453c0a024410993f64a90ab85da317018fe520
Mohd Akram Ansari <mdakram28@gmail.com>
2023-05-07 06:39:59 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (python3)

--- my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.py
+++ my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.py
@@ -6,33 +6,28 @@
 #         self.right = None
 
 class Solution:
-    def findStack(self, node, val, st):
-        if node is None:
-            return False
-        if node.val == val:
-            st.append(node)
-            return True
-        elif self.findStack(node.left, val, st) or self.findStack(node.right, val, st):
-            st.append(node)
-            return True
-        else:
-            return False
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        st_p = []
-        st_q = []
 
-        self.findStack(root, p.val, st_p)
-        self.findStack(root, q.val, st_q)
 
-        st_p.reverse()
-        st_q.reverse()
+        def find(at, node, st):
+            if at is None: return False
+            if at == node or find(at.left, node, st) or find(at.right, node, st): 
+                st.append(at)
+                return True
+            return False
+        
 
-        # print(list(map(lambda s: s.val, st_p)))
-        # print(list(map(lambda s: s.val, st_q)))
-
+        P = []
+        Q = []
+        find(root, p, P)
+        find(root, q, Q)
+        P.reverse()
+        Q.reverse()
+        # print([n.val for n in P], [n.val for n in Q])
+        
         i = 0
-        min_len = min(len(st_p), len(st_q))
-        while i < min_len and st_p[i] == st_q[i]:
+        lim = min(len(P), len(Q))
+
+        while i < lim and P[i] == Q[i]:
             i += 1
-        return st_p[i-1]
+        return P[i-1]
