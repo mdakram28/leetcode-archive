49b5178940f45598283b8e190e0c29d577220e56
Mohd Akram Ansari <mdakram28@gmail.com>
2023-05-08 01:54:39 +0000

    Sync LeetCode submission - Matrix Diagonal Sum (python3)

---
+++ my-folder/problems/matrix_diagonal_sum/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        ans = 0
+        for i in range(n):
+            ans += mat[i][i] + mat[i][n-i-1]
+        
+        if n%2:
+            ans -= mat[n//2][n//2]
+        return ans
