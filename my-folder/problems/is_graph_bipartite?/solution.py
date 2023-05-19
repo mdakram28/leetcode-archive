c43a4c88a84219ec56c5c0cf3cb275d434e31d2c
Mohd Akram Ansari <mdakram28@gmail.com>
2023-05-19 00:24:26 +0000

    Sync LeetCode submission - Is Graph Bipartite? (python3)

--- my-folder/problems/is_graph_bipartite?/solution.py
+++ my-folder/problems/is_graph_bipartite?/solution.py
@@ -3,19 +3,35 @@ class Solution:
         n = len(graph)
         color = [0] * n
 
-        def check_node(at, c):
-            if color[at]:
-                return color[at] == c
-            color[at] = c
-            c = 3-c
-            for to in graph[at]:
-                if not check_node(to, c):
-                    return False
-            return True
+        # def check_node(at, c):
+        #     if color[at]:
+        #         return color[at] == c
+        #     color[at] = c
+        #     c = 3-c
+        #     for to in graph[at]:
+        #         if not check_node(to, c):
+        #             return False
+        #     return True
         
+        q = []
+        qi = 0
+
         for i in range(n):
-            if not color[i] and not check_node(i, 1):
-                return False
+            if color[i]: continue
+
+            q.append(i)
+            color[i] = 1
+
+            while qi < len(q):
+                at = q[qi]
+                qi += 1
+                c = 3-color[at]
+                for to in graph[at]:
+                    if color[to]:
+                        if color[to] != c: return False
+                    else:
+                        color[to] = c
+                        q.append(to)
         
         return True
 
