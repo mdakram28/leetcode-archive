2b94e8637451aefc91627f30bd818aff2e93cf92
Mohd Akram Ansari <mdakram28@gmail.com>
2023-05-20 23:25:36 +0000

    Sync LeetCode submission - Find The Original Array of Prefix Xor (python3)

---
+++ my-folder/problems/find_the_original_array_of_prefix_xor/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        arr = [pref[0]]
+        for i in range(1, len(pref)):
+            arr.append(pref[i]^pref[i-1])
+        return arr
