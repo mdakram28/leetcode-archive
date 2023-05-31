46b51ddbce053611cf1c0379cfdb0a5190af40d5
Mohd Akram Ansari <mdakram28@gmail.com>
2023-05-31 23:16:24 +0000

    Sync LeetCode submission - Sqrt(x) (cpp)

--- my-folder/problems/sqrt(x)/solution.cpp
+++ my-folder/problems/sqrt(x)/solution.cpp
@@ -1,22 +1,18 @@
-#define debug(var1) printf( #var1 "=%d\n", var1)
-#define debug(var1,var2) printf( #var1 "=%d, " #var2 "=%d\n", var1, var2)
-#define debug(var1,var2,var3) printf( #var1 "=%d, " #var2 "=%d, " #var3 "=%d\n", var1, var2, var3)
-
 class Solution {
 public:
     int mySqrt(int x) {
-        long l = 0;
-        long r = x/2;
-        while(l<=r) {
-            long mid = (l+r)/2;
-            // debug(l, r, mid);
-            if ((mid*mid) > x) {
-                r = mid-1;
+        long long lo = 0;
+        long long hi = INT_MAX;
+        while (lo<hi) {
+            long long mid = (lo+hi)/2;
+            if ((mid*mid) < x) {
+                lo = mid+1;
+            } else if ((mid*mid) > x) {
+                hi = mid;
             } else {
-                l = mid+1;
+                return mid;
             }
         }
-        if ((l*l) > x) return l-1;
-        else return l;
+        return lo-1;
     }
 };
