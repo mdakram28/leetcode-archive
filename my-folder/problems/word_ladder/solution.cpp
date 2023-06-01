866ea3d0f65d73b3e8ef1611213684ce2425e912
Mohd Akram Ansari <mdakram28@gmail.com>
2023-06-01 04:22:54 +0000

    Sync LeetCode submission - Word Ladder (cpp)

--- my-folder/problems/word_ladder/solution.cpp
+++ my-folder/problems/word_ladder/solution.cpp
@@ -1,64 +1,53 @@
 class Solution {
-    void check_nodes(int i, int d,
-                     vector<vector<int>> &graph, 
-                     vector<int> &distance) {
-        if(d < distance[i]) {
-            distance[i] = d;
-            for(int &conn: graph[i]) {
-                check_nodes(conn, d+1, graph, distance);
-            }
-        }
-    }
-    
-    int word_diff(string &w1, string &w2) {
-        int c = 0;
-        for(int i=0;i<w1.length();i++) {
-            if(w1[i] != w2[i]) {c++;}
-        }
-        return c;
-    }
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        vector<int> row;
-        vector<vector<int>> graph(wordList.size(), row);
-        vector<int> distance(wordList.size(), INT_MAX);
-        
-        for(int i=0;i<wordList.size();i++) {
-            
-            for(int j=0;j<i;j++) {
-                if(word_diff(wordList[i], wordList[j]) == 1) {
-                    graph[j].push_back(i);
-                    graph[i].push_back(j);
-                }
-            }
-            
+        if(find(wordList.begin(), wordList.end(), beginWord) == wordList.end()) {
+            wordList.push_back(beginWord);
         }
-        
-        for(int i=0;i<wordList.size();i++) {
-            if(word_diff(wordList[i], beginWord) == 1) {
-                check_nodes(i, 1, graph, distance);
-            }
-        }
-        
-        // for(int i=0;i<wordList.size();i++) {
-        //     cout << wordList[i] << "=" << distance[i] << "\t";
+
+        // for(auto w: wordList) {
+        //     cout << w << ", ";
         // }
         // cout << endl;
-        // for(int i=0;i<graph.size();i++) {
-        //     cout << wordList[i] << " : ";
-        //     for(int j=0;j<graph[i].size();j++) {
-        //         cout << wordList[j] << ", " ;
-        //     }
-        //     cout << endl;
-        // }
-        
-        for(int i=0;i<wordList.size();i++) {
-            if(wordList[i] == endWord) {
-                if(distance[i] == INT_MAX) return 0;
-                else return distance[i]+1;
+
+        // int n = wordList.size();
+        map<string, vector<string>> g;
+
+        for(string &w1: wordList) {
+            for(string &w2: wordList) {
+                int diff = 0;
+                for(int i=0; diff<=1 && i<w1.length() && i<w2.length(); i++) {
+                    if (w1[i]!=w2[i]) diff++;
+                }
+                if (diff <= 1) {
+                    g[w1].push_back(w2);
+                }
             }
         }
-        
-        return 0;
+
+        map<string, int> dist;
+        priority_queue<pair<int, string>, vector<pair<int, string>>, greater<>> q;
+        dist[beginWord] = 1;
+        q.push(make_pair(1, beginWord));
+
+        while (q.size() > 0) {
+            int d;
+            string at;
+            tie (d, at) = q.top();
+            q.pop();
+            d++;
+            for(string &to: g[at]) {
+                if (dist.find(to) == dist.end() || d < dist[to]) {
+                    dist[to] = d;
+                    q.push(make_pair(d, to));
+                }
+            }
+        }
+
+        // for(auto w: dist) {
+        //     printf("%s : %d \n", w.first.c_str(), w.second);
+        // }
+
+        return dist[endWord] != INT_MAX ? dist[endWord] : 0;
     }
 };
