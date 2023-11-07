ce8d3062ccb9b934af1865c665d1414548591c18
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-07 17:42:15 +0000

    Sync LeetCode submission - Set Matrix Zeroes (python3)

--- my-folder/problems/set_matrix_zeroes/solution.py
+++ my-folder/problems/set_matrix_zeroes/solution.py
@@ -3,32 +3,44 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rz = 0 in matrix[0]
-        cz = False
-        for r in matrix:
-            if r[0] == 0:
-                cz = True
-        rows = len(matrix)
-        cols = len(matrix[0])
-        for r in range(1, rows):
-            for c in range(1, cols):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        r0 = False
+        c0 = False
+
+        for r in range(1, m):
+            if matrix[r][0] == 0:
+                c0 = True
+        
+        for c in range(1, n):
+            if matrix[0][c] == 0:
+                r0 = True
+        
+        if matrix[0][0] == 0:
+            r0 = True
+            c0 = True
+
+        for r in range(1, m):
+            for c in range(1, n):
                 if matrix[r][c] == 0:
                     matrix[0][c] = 0
                     matrix[r][0] = 0
-        for c in range(1, cols):
-            if matrix[0][c] == 0:
-                for r in range(1, rows):
-                    matrix[r][c] = 0
-                    
-        for r in range(1, rows):
+        
+        for r in range(1, m):
             if matrix[r][0] == 0:
-                for c in range(1, cols):
+                for c in range(n):
                     matrix[r][c] = 0
         
-        if rz:
-            for c in range(0, cols):
-                matrix[0][c] = 0
-        if cz:
-            for r in range(0, rows):
-                matrix[r][0] = 0
+        for c in range(1, n):
+            if matrix[0][c] == 0:
+                for r in range(m):
+                    matrix[r][c] = 0
         
+        if r0:
+            for c in range(n):
+                matrix[0][c] = 0
+
+        if c0:
+            for r in range(m):
+                matrix[r][0] = 0
