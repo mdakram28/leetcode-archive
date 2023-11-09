92c785538abc73f2970109d88209e3658f61bef3
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-09 20:10:32 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

--- my-folder/problems/product_of_array_except_self/solution.py
+++ my-folder/problems/product_of_array_except_self/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = []
-        prefix_prod = 1
-        for i in range(len(nums)):
-            result.append(prefix_prod)
-            prefix_prod *= nums[i]
+        prod = 1
+        suff = []
+        for num in nums[::-1]:
+            suff.append(prod)
+            prod *= num
+        suff.reverse()
+
+        prod = 1
+        for i, num in enumerate(nums):
+            suff[i] *= prod
+            prod *= num
         
-        suffix_prod = 1
-        for i in range(len(nums)-1, -1, -1):
-            result[i] *= suffix_prod
-            suffix_prod *= nums[i]
-        
-        return result
+        return suff
