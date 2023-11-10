ed6267dc901bc2abb31f524296505863cc3de047
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-10 07:35:04 +0000

    Sync LeetCode submission - Unique Number of Occurrences (python3)

--- my-folder/problems/unique_number_of_occurrences/solution.py
+++ my-folder/problems/unique_number_of_occurrences/solution.py
@@ -1,10 +1,6 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        count = defaultdict(int)
+        f = defaultdict(int)
         for num in arr:
-            count[num] += 1
-        found = set()
-        for f in count.values():
-            if f in found: return False
-            found.add(f)
-        return True
+            f[num] += 1
+        return len(set(f.values())) == len(f)
