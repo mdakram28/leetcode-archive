dd69cd704ad99920b31a5cb54ecf98f4241be300
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-13 20:47:26 +0000

    Sync LeetCode submission - Sliding Window Median (python3)

--- my-folder/problems/sliding_window_median/solution.py
+++ my-folder/problems/sliding_window_median/solution.py
@@ -1,32 +1,91 @@
-from sortedcontainers import SortedList
+# from sortedcontainers import SortedList
+class Heap():
+    def __init__(self):
+        self._q = []
+        self._rem = defaultdict(int)
+        self._remc = 0
+    
+    def __len__(self):
+        return len(self._q)-self._remc
+
+class MinHeap(Heap):
+    def add(self, num):
+        heappush(self._q, num)
+    
+    def remove(self, num):
+        self._rem[num] += 1
+        self._remc += 1
+    
+    def min(self):
+        while self._q and self._rem[self._q[0]] > 0:
+            self._rem[heappop(self._q)] -= 1
+            self._remc -= 1
+        return self._q[0] if self._q else None
+    
+    def pop(self):
+        while self._q and self._rem[self._q[0]] > 0:
+            self._rem[heappop(self._q)] -= 1
+            self._remc -= 1
+        return heappop(self._q) if self._q else None
+
+class MaxHeap(Heap):
+    def add(self, num):
+        heappush(self._q, -num)
+    
+    def remove(self, num):
+        self._rem[num] += 1
+        self._remc += 1
+    
+    def max(self):
+        while self._q and self._rem[-self._q[0]] > 0:
+            self._rem[-heappop(self._q)] -= 1
+            self._remc -= 1
+        return -self._q[0] if self._q else None
+        
+    def pop(self):
+        while self._q and self._rem[-self._q[0]] > 0:
+            self._rem[-heappop(self._q)] -= 1
+            self._remc -= 1
+        return -heappop(self._q) if self._q else None
 
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        left = SortedList()
-        right = SortedList()
-        n = len(nums)
-        
-        for i in range(k-1):
-            right.add(nums[i])
-        
-        while len(right)-len(left) > 1:
-            left.add(right.pop(0))
-        
-        ans = []
-        for i in range(k-1, n):
-            right.add(nums[i])
-            left.add(right.pop(0))
+        left = MaxHeap()
+        right = MinHeap()
 
-            if len(left) - len(right) > 1:
-                right.add(left.pop(-1))
+        def balance():
+            while len(left) > len(right):
+                right.add(left.pop())
             
-            med = left[-1] if k%2 else (left[-1] + right[0])/2
-            ans.append(med)
+            while len(right) > len(left)+1:
+                left.add(right.pop())
 
-            rem = nums[i-k+1]
-            if rem <= med:
-                left.remove(rem)
+        def add(num):
+            if len(left) > 0 and num <= left.max():
+                left.add(num)
             else:
-                right.remove(rem)
+                right.add(num)
+            balance()
+
+        def remove(num):
+            if len(left)>0 and num <= left.max():
+                left.remove(num)
+            else:
+                right.remove(num)
+            balance()
+
+        def median():
+            if len(left) == len(right):
+                return (left.max() + right.min())/2
+            else:
+                return right.min()
+
+        ans = []
+        for i, num in enumerate(nums):
+            add(num)
+            if i >= k:
+                remove(nums[i-k])
+            if i >= (k-1):
+                ans.append(median())
         
         return ans
