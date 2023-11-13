b420e0a40f4bb4243d41f415885e537550ee715c
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-13 06:50:29 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (python3)

--- my-folder/problems/longest_increasing_subsequence/solution.py
+++ my-folder/problems/longest_increasing_subsequence/solution.py
@@ -1,35 +1,11 @@
+from sortedcontainers import SortedList
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        piles = []
-
-        def bs(target):
-            l = 0
-            r = len(piles)
-            while l<r:
-                mid = (l+r) // 2
-                if target > piles[mid]:
-                    l = mid+1
-                else:
-                    r = mid
-            return l
-        
+        ans = []
         for num in nums:
-            pi = bs(num)
-            if pi < len(piles):
-                piles[pi] = num
+            i = bisect_left(ans, num)
+            if i == len(ans):
+                ans.append(num)
             else:
-                piles.append(num)
-        return len(piles)
-    
-    def lengthOfLIS_DP(self, nums: List[int]) -> int:
-        dp = []
-        max_count = 1
-        for i in range(len(nums)):
-            num = nums[i]
-            count = 1
-            for j in range(i):
-                if nums[j] < num:
-                    count = max(count, dp[j]+1)
-            dp.append(count)
-            max_count = max(max_count, count)
-        return max_count
+                ans[i] = num
+        return len(ans)
