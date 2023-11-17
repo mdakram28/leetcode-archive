4cadb60a4ff3a81c6029b2cca42b6339cdaae660
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-17 16:32:22 +0000

    Sync LeetCode submission - Non-overlapping Intervals (python3)

--- my-folder/problems/non-overlapping_intervals/solution.py
+++ my-folder/problems/non-overlapping_intervals/solution.py
@@ -1,25 +1,13 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        counts = [0] * (len(intervals)+1)
 
-        def search_start(l, t):
-            r = len(intervals)
-            while r>l:
-                mid = (l+r)//2
-                if intervals[mid][0] < t:
-                    l = mid+1
-                else:
-                    r = mid
-            # print(intervals[l:], f"{t=}, {l=}, {r=}")
-            return r
+        ends = SortedList([(-float('inf'),0)])
 
-        max_count = 0
-        for i in range(len(intervals)-1, -1, -1):
-            start, end = intervals[i]
-            next_idx = search_start(i+1, end)
-            max_count = max(max_count, counts[next_idx] + 1)
-            counts[i] = max_count
-            # print(f"{i=}, {max_count=}")
-        
-        return len(intervals) - max_count
+        ans = 0
+        for s, e in sorted(intervals, key=lambda i: i[1]):
+            prev_maxint = ends[ends.bisect_left((s+1, 0))-1][1]
+            ans = max(ans, 1+ prev_maxint)
+            ends.add((e, ans))
+        return len(intervals)-ans
