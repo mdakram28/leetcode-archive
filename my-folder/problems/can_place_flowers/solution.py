28ad00c0ce6461e10bdbe3882527d1cec12cbdb8
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-17 19:53:51 +0000

    Sync LeetCode submission - Can Place Flowers (python3)

--- my-folder/problems/can_place_flowers/solution.py
+++ my-folder/problems/can_place_flowers/solution.py
@@ -1,22 +1,10 @@
-from math import ceil
 class Solution:
-    def canPlaceFlowers(self, flowers: List[int], place: int) -> bool:
-        
-        i = 0
-        n = len(flowers)
-        slots = 0
-        while i < n:
-            if flowers[i] == 1:
-                i += 1 
-            else:
-                start = i
-                while i < n and flowers[i] == 0:
-                    i += 1
-                num = i-start
-                slots += (num-1)//2
-                if i == n and start == 0:
-                   slots += 1
-                elif num%2 == 0 and (i == n or start == 0):
-                    slots += 1
-        
-        return place <= slots
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        for i, f in enumerate(flowerbed):
+            if f == 0:
+                if (i == 0 or flowerbed[i-1] == 0) and (i==len(flowerbed)-1 or flowerbed[i+1]==0):
+                    n -= 1
+                    flowerbed[i] = 1
+        return n <= 0
+                
