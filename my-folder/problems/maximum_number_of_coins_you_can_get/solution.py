59780a78f951266962fce35e0a4bf78b64561d4e
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-24 02:36:32 +0000

    Sync LeetCode submission - Maximum Number of Coins You Can Get (python3)

--- my-folder/problems/maximum_number_of_coins_you_can_get/solution.py
+++ my-folder/problems/maximum_number_of_coins_you_can_get/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         piles.sort(reverse=True)
-        return sum(piles[i*2+1] for i in range(len(piles)//3))
+        return sum(piles[i] for i in range(1, (len(piles)//3)*2+1, 2))
