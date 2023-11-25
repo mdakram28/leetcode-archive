1441a2141d47cce38def5a95b82372b57c075e99
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-25 18:38:58 +0000

    Sync LeetCode submission - Longest Alternating Subarray (python3)

--- my-folder/problems/longest_alternating_subarray/solution.py
+++ my-folder/problems/longest_alternating_subarray/solution.py
@@ -5,12 +5,11 @@ class Solution:
             count = 1
             diff = 1
             for j in range(i+1, len(nums)):
-                if nums[j]-nums[j-1] == diff:
-                    count += 1
-                    diff = -diff
-                else:
+                if nums[j] - nums[j-1] != diff:
                     break
-            # print(i, count)
+                diff = -diff
+                count += 1
             if count > 1:
                 ans = max(ans, count)
+        
         return ans
