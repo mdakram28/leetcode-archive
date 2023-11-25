e924f7719254bd664407ed64c27438b2f3336587
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-25 18:41:02 +0000

    Sync LeetCode submission - Relocate Marbles (python3)

--- my-folder/problems/relocate_marbles/solution.py
+++ my-folder/problems/relocate_marbles/solution.py
@@ -1,15 +1,9 @@
 class Solution:
     def relocateMarbles(self, nums: List[int], moveFrom: List[int], moveTo: List[int]) -> List[int]:
-        count = defaultdict(int)
-        for num in nums:
-            count[num] += 1
-            
-        for f,t in zip(moveFrom, moveTo):
-            if f == t: continue
-            count[t] = count[t]+count[f]
-            del count[f]
+        occ = set(nums)
+        for a, b in zip(moveFrom, moveTo):
+            if a in occ:
+                occ.remove(a)
+                occ.add(b)
         
-        # allstones = []
-        # for pos, c in count.items():
-        #     allstones.extend([pos]*c)
-        return sorted(count.keys())
+        return list(sorted(list(occ)))
