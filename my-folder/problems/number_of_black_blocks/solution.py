9a90ca7c542927a532a5d33f9c53708b9a46788a
Mohd Akram Ansari <mdakram28@gmail.com>
2023-11-25 18:59:58 +0000

    Sync LeetCode submission - Number of Black Blocks (python3)

--- my-folder/problems/number_of_black_blocks/solution.py
+++ my-folder/problems/number_of_black_blocks/solution.py
@@ -1,22 +1,16 @@
 class Solution:
     def countBlackBlocks(self, m: int, n: int, coordinates: List[List[int]]) -> List[int]:
-        coords = set((r,c) for r,c in coordinates)
-        tocount = set()
-        for r,c in coordinates:
-            for dr, dc in (0, 0), (0, -1), (-1, 0), (-1, -1):
-                nr,nc = r+dr, c+dc
-                # print(nr, nc)
-                if nr>=0 and nr<(m-1) and nc>=0 and nc<(n-1):
-                    tocount.add((nr,nc))
-        
-        print(tocount)
+        count = {}
         ans = [0]*5
-        for r, c in tocount:
-            count = 0
-            for dr, dc in (0, 0), (0, 1), (1, 0), (1, 1):
-                nr, nc = r+dr, c+dc
-                if (nr,nc) in coords:
-                    count += 1
-            ans[count] += 1
-        ans[0] = (m-1)*(n-1) - sum(ans)
+        ans[0] = (m-1)*(n-1)
+        
+        for r, c in coordinates:
+            for dr, dc in (-1, -1), (-1, 0), (0, -1), (0, 0):
+                r2, c2 = r+dr, c+dc
+                if r2 < m-1 and r2 >= 0 and c2 < n-1 and c2 >= 0:
+                    pc = count.get((r2, c2), 0)
+                    ans[pc] -= 1
+                    ans[pc+1] += 1
+                    count[(r2, c2)] = pc+1
+        
         return ans
