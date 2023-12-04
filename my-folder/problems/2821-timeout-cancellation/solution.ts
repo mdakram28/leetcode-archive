0dbb3e074bc8a60de9ce7577099ed7aacbdd5e5c
Mohd Akram Ansari <mdakram28@gmail.com>
2023-12-04 18:55:47 +0000

    Sync LeetCode submission Runtime - 72 ms (24.34%), Memory - 43.6 MB (8.95%)

---
+++ my-folder/problems/2821-timeout-cancellation/solution.ts
@@ -0,0 +1,39 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+    setTimeout(() => {
+        if (fn)
+            fn(...args);
+    }, t);
+
+	return () => {
+        fn = undefined;
+    };
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const start = performance.now() 
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)})
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelT)
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *
+ *  setTimeout(() => {
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
