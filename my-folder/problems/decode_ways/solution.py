94e968131a33449ae473200081a8652abd8541f2
Mohd Akram Ansari <mdakram28@gmail.com>
2023-12-25 00:33:04 +0000

    Sync LeetCode submission - Decode Ways (python3)

--- my-folder/problems/decode_ways/solution.py
+++ my-folder/problems/decode_ways/solution.py
@@ -1,14 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        DP = [0] * (len(s) + 1)
-        DP[0] = 1
-        for i in range(1, len(s) + 1):
-            DP[i] = DP[i-1] if s[i-1] != '0' else 0
-            if i > 1 and s[i-2] != '0':
-                if s[i-1] <= '6':
-                    if s[i-2] <= '2':
-                        DP[i] += DP[i-2]
-                elif s[i-2] <= '1':
-                    DP[i] += DP[i-2]
+        n = len(s)
+        b = 1
+        a = 1 if s[-1] != '0' else 0
+
+        for i in range(n-2, -1, -1):
+            if s[i] == '0':
+                a, b = 0, a
+            elif s[i] == '1' or (s[i]=='2' and s[i+1] <= '6'):
+                a, b = a+b, a
+            else:
+                a, b = a, a
         
-        return DP[-1]
+        return a
+                
+            
