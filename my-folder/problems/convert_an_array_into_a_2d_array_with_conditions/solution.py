304a9ce7f6260fdf08329663d395c19edd0ea65d
Mohd Akram Ansari <mdakram28@gmail.com>
2024-01-02 00:26:06 +0000

    Sync LeetCode submission - Convert an Array Into a 2D Array With Conditions (python3)

--- my-folder/problems/convert_an_array_into_a_2d_array_with_conditions/solution.py
+++ my-folder/problems/convert_an_array_into_a_2d_array_with_conditions/solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        freq = collections.defaultdict(int)
-        for num in nums:
-            freq[num] += 1
-        ret = [[] for _ in range(max(freq.values()))]
-        for num, f in freq.items():
-            for r in range(f):
-                ret[r].append(num)
+        count = defaultdict(int)
+        ret = []
+
+        for val in nums:
+            i = count[val]
+            count[val] += 1
+            if i >= len(ret):
+                ret.append([])
+            ret[i].append(val)
+        
         return ret
