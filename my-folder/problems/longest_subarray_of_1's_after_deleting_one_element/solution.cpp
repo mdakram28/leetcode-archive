f69267895ee38390776ecd56cfc39a107a7943e3
Mohd Akram Ansari <mdakram28@gmail.com>
2024-01-11 23:25:28 +0000

    Sync LeetCode submission - Longest Subarray of 1's After Deleting One Element (cpp)

--- my-folder/problems/longest_subarray_of_1's_after_deleting_one_element/solution.cpp
+++ my-folder/problems/longest_subarray_of_1's_after_deleting_one_element/solution.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int i=0;
-        int n = nums.size();
-        while(i<n && nums[i]==1) i++;
-        if (i==n) return n-1;
-        i++;
-
-        int start = 0;
-        int ans = i;
-        
-        while (i<n) {
-            if (nums[i] == 0) {
-                ans = max(ans, i-start);
-                
-                while(nums[start]==1) {
-                    start++;
-                }
-                start++;
+        int l=0, r=0;
+        const int n = nums.size();
+        int z = 0;
+        int ans = 0;
+        while (r < n) {
+            if (nums[r++] == 0) {
+                z++;
             }
-            i++;
+            while (z > 1) {
+                if(nums[l] == 0) {
+                    z--;
+                }
+                l++;
+            }
+            ans = std::max(ans, r-l-1);
         }
-        // printf("[%d:%d]\n", start, i);
-        ans = max(ans, i-start);
-        return ans-1;
+        return ans;
     }
 };
