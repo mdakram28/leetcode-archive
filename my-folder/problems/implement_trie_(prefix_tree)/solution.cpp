2360f4dfb65473155300489ae52a5ea917c502f6
Mohd Akram Ansari <mdakram28@gmail.com>
2024-01-12 04:49:34 +0000

    Sync LeetCode submission - Implement Trie (Prefix Tree) (cpp)

--- my-folder/problems/implement_trie_(prefix_tree)/solution.cpp
+++ my-folder/problems/implement_trie_(prefix_tree)/solution.cpp
@@ -1,11 +1,33 @@
-const int ALPHABET_SIZE = 26;
 
-// trie node
-struct TrieNode
-{
-    struct TrieNode *children[ALPHABET_SIZE];
-    bool isEndOfWord;
+struct TrieNode {
+    std::unordered_map<char, TrieNode*> children;
+    bool isEnd = false;
+    void insert(std::string &word) {
+        auto node = this;
+        for(char c: word) {
+            if (!node->children[c]) {
+                node->children[c] = new TrieNode();
+            }
+            node = node->children[c];
+        }
+        node->isEnd = true;
+    }
+
+    bool find(std::string &word, bool matchEnd) {
+        auto node = this;
+        for(int i=0; i<word.size() && node; i++) {
+            node = node->children[word[i]];
+        }
+        return node != nullptr && (!matchEnd || node->isEnd);
+    }
+
+    ~TrieNode() {
+        for(auto &[c, next]: children) {
+            delete next;
+        }
+    }
 };
+
 
 class Trie {
     TrieNode *root;
@@ -13,37 +35,21 @@ public:
     Trie() {
         root = new TrieNode();
     }
+
+    ~Trie() {
+        delete root;
+    }
     
     void insert(string word) {
-        TrieNode *node = root;
-        for(int i=0;i<word.length();i++) {
-            int index = word[i] - 'a';
-            if (node->children[index]) {
-                node = node->children[index];
-            } else {
-                node->children[index] = new TrieNode();
-                node = node->children[index];
-            }
-        }
-        node->isEndOfWord = true;
+        root->insert(word);
     }
     
     bool search(string word) {
-        TrieNode *node = root;
-        for(int i=0; i<word.length() && node!=nullptr; i++) {
-            int index = word[i] - 'a';
-            node = node->children[index];
-        }
-        return node!=nullptr && node->isEndOfWord;
+        return root->find(word, true);
     }
     
     bool startsWith(string prefix) {
-        TrieNode *node = root;
-        for(int i=0; i<prefix.length() && node!=nullptr; i++) {
-            int index = prefix[i] - 'a';
-            node = node->children[index];
-        }
-        return node!=nullptr;
+        return root->find(prefix, false);
     }
 };
 
