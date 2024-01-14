1fe1fbd34a2579cdc669ceddee4f2bfe9d3a0cf9
Mohd Akram Ansari <mdakram28@gmail.com>
2024-01-14 19:55:40 +0000

    Sync LeetCode submission - Find Pivot Index (cpp)

--- my-folder/problems/find_pivot_index/solution.cpp
+++ my-folder/problems/find_pivot_index/solution.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    int pivotIndex(vector<int> &nums) {
-//        auto size = nums.size();
-        long sum_total = accumulate(nums.begin(), nums.end(), 0);
-        long sum_left = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if(!(2*sum_left - sum_total + nums[i])) {
-                return i;
-            }
-            sum_left += nums[i];
+    int pivotIndex(vector<int>& nums) {
+        long right = std::reduce(nums.begin(), nums.end());
+        long left = 0;
+        for(int i=0; i<nums.size(); i++) {
+            right -= nums[i];
+            if (left == right) return i;
+            left += nums[i];
         }
         return -1;
     }
