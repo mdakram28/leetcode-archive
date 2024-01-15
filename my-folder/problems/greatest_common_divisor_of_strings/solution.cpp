e2ec824304ce91839d1398ff5234b3b296d52484
Mohd Akram Ansari <mdakram28@gmail.com>
2024-01-15 04:01:22 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings (cpp)

--- my-folder/problems/greatest_common_divisor_of_strings/solution.cpp
+++ my-folder/problems/greatest_common_divisor_of_strings/solution.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        int n1 = str1.length(), n2 = str2.length();
-        
-        int limAns = min(n1, n2);
-        
-        for(int i=limAns; i>0; i--) {
-            if (n1%i || n2%i) continue;
-            bool matched = true;
-
-            for(int j=0; j<n1 && matched; j++) {
-                if (str1[j] != str1[j%i]) 
-                    matched = false;
+        int maxL = std::min(str1.length(), str2.length());
+        for (int l=maxL; l>0; l--) {
+            if (str1.length()%l != 0 || str2.length()%l != 0)
+                continue;
+            
+            bool match = true;
+            for(int j=l; j<str1.length(); j++) {
+                if (str1[j] != str1[j%l]) {
+                    match = false;
+                    break;
+                }
             }
 
-            for(int j=0; j<n2 && matched; j++) {
-                if (str2[j] != str1[j%i]) 
-                    matched = false;
-            }
 
-            if (matched) return str1.substr(0, i);
+            if (match) {
+                for(int j=0; j<str2.length(); j++) {
+                    // cout << l << ", " << j << endl;
+                    if (str2[j] != str1[j%l]) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) return str1.substr(0, l);
+            }
 
         }
         return "";
