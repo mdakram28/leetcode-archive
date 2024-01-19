365bd603f682f29066b6e4d2b21389c57a2dc5e2
Mohd Akram Ansari <mdakram28@gmail.com>
2024-01-19 00:18:12 +0000

    Sync LeetCode submission - Minimum Falling Path Sum (python3)

--- my-folder/problems/minimum_falling_path_sum/solution.py
+++ my-folder/problems/minimum_falling_path_sum/solution.py
@@ -1,19 +1,14 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        n = len(matrix)
-        cache = [[None]*n for r in range(n-1)] + [matrix[-1]]
+        m, n = len(matrix), len(matrix[0])
+        for r in range(m):
+            matrix[r].append(float('inf'))
+        for r in range(m-2, -1, -1):
+            for c in range(n):
+                matrix[r][c] += min(
+                    matrix[r+1][c-1],
+                    matrix[r+1][c],
+                    matrix[r+1][c+1]
+                )
         
-        def dfs(r, c):
-            if c < 0 or c >= n: return float('inf')
-            if cache[r][c] is not None: 
-                return cache[r][c]
-            # prev += matrix[r][c]
-            ans = matrix[r][c] + min(
-                dfs(r+1, c-1),
-                dfs(r+1, c),
-                dfs(r+1, c+1)
-            )
-            cache[r][c] = ans
-            return ans
-        
-        return min(dfs(0, c) for c in range(n))            
+        return min(matrix[0])
