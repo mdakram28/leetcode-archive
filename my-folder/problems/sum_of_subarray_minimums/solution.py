64cffc71c8d3943d83525273224ceaf623237cb0
Mohd Akram Ansari <mdakram28@gmail.com>
2024-01-20 01:03:33 +0000

    Sync LeetCode submission - Sum of Subarray Minimums (python3)

--- my-folder/problems/sum_of_subarray_minimums/solution.py
+++ my-folder/problems/sum_of_subarray_minimums/solution.py
@@ -1,28 +1,28 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         n = len(arr)
-        arr.append(0)
-        left_spans = [0]*n
-        # right_spans = [0]*n
-        st = [-1]
-        mod = 10**9 + 7
+        mod = (10**9+7)
+        arr.append(-float('inf'))
 
-        for i in range(n):
+        st = [n]
+        next_i = [0]*n
+        for i in range(n-1, -1, -1):
             while arr[st[-1]] >= arr[i]:
                 st.pop()
-            left_spans[i] = i-st[-1]
+            
+            next_i[i] = st[-1]
             st.append(i)
         
-        st.clear()
-        st.append(n)
-        ret = 0
-        for i in range(n-1, -1, -1):
+        st = [-1]
+        total = 0
+        for i in range(n):
             while arr[st[-1]] > arr[i]:
                 st.pop()
-            # right_spans[i] = st[-1]-i
-            ret = (ret + left_spans[i]*(st[-1]-i)*arr[i]) % mod
+            
+            total = (total + arr[i]*(i-st[-1])*(next_i[i]-i))%mod
+
             st.append(i)
-
-        # print(left_spans, right_spans)
-
-        return ret
+        
+        return total
