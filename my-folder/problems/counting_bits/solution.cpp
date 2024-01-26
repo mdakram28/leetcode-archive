7ef32d22c1292a1043dc4f1dbf82d2b3fdd88e73
Mohd Akram Ansari <mdakram28@gmail.com>
2024-01-26 20:09:39 +0000

    Sync LeetCode submission - Counting Bits (cpp)

--- my-folder/problems/counting_bits/solution.cpp
+++ my-folder/problems/counting_bits/solution.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ret(n+1);
-        ret[0] = 0;
-        if (n == 0) return ret;
-        ret[1] = 1;
-        for(int i=2;i<=n;i++) {
-            ret[i] = ret[i>>1] + (i&1);
+        std::vector<int> ans(n+1);
+        for(int i=1; i<=n; i++) {
+            ans[i] = ans[i & (i-1)]+1;
         }
-        return ret;
+        return ans;
     }
 };
